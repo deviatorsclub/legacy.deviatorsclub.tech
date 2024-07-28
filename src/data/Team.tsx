c8954232aeab6dff060cbc1932b00782c90722d3
Pulkit <kpulkit15234@gmail.com>
2024-07-28 16:37:51 +0530

    about section

--- src/data/Team.tsx
+++ src/data/Team.tsx
@@ -9,11 +9,16 @@ import Manas from "../assets/team/manas.webp";
 import Vidushi from "../assets/team/vidushi.webp";
 import Kunal from "../assets/team/kunal.webp";
 import Tanmay from "../assets/team/tanmay.webp";
+import Pooja from "../assets/team/pooja.webp";
+import Dipti from "../assets/team/dipti.webp";
+import Divyansh from "../assets/team/divyansh.webp";
+import Saurabh from "../assets/team/saurabh.webp";
+import Yash from "../assets/team/yash.webp";
 
 export default [
   {
     name: "Kanak Tanwar",
-    position: "Leader",
+    position: "Leader & AI lead",
     intro:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet quisquam fugiat eum nulla, accusamus rerum.",
     urls: {
@@ -29,11 +34,15 @@ export default [
     position: "Vice President",
     intro:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet quisquam fugiat eum nulla, accusamus rerum.",
+    urls: {
+      github: "https://github.com/Vivek09Chahal",
+      linkedin: "https://www.linkedin.com/in/vivekchahal09/",
+    },
     image: Vivek,
   },
   {
     name: "Pulkit",
-    position: "Chairperson",
+    position: "Chairperson & Web Lead",
     intro:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet quisquam fugiat eum nulla, accusamus rerum.",
     urls: {
@@ -46,14 +55,18 @@ export default [
   },
   {
     name: "Krishna Kartikay Bhatt",
-    position: "Events Lead",
+    position: "Events Lead & Cloud Lead",
     intro:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet quisquam fugiat eum nulla, accusamus rerum.",
+    urls: {
+      linkedin: "https://www.linkedin.com/in/krishna-kartikay-bhatt/",
+      github: "https://github.com/kkbhatt07",
+    },
     image: KK,
   },
   {
     name: "Ayush Goyal",
-    position: "Vlunteer Coordinator",
+    position: "Volunteer Coordinator & Web Co-leader",
     intro:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet quisquam fugiat eum nulla, accusamus rerum.",
     image: Ayush,
@@ -68,27 +81,48 @@ export default [
     position: "Women in Tech",
     intro:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet quisquam fugiat eum nulla, accusamus rerum.",
+    urls: {
+      github: "https://github.com/deepikaa0402",
+      linkedin: "www.linkedin.com/in/deepika-anand-4a371a26b",
+    },
     image: Deepika,
   },
   {
     name: "Diksha Sharma",
-    position: "Design",
+    position: "Design lead",
     intro:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet quisquam fugiat eum nulla, accusamus rerum.",
     image: Diksha,
   },
   {
-    name: "Manas Thakur",
-    position: "AI Team",
+    name: "Pooja",
+    position: "Design Team",
     intro:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet quisquam fugiat eum nulla, accusamus rerum.",
+    image: Pooja,
+  },
+  {
+    name: "Manas Thakur",
+    position: "AIML Team",
+    intro:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet quisquam fugiat eum nulla, accusamus rerum.",
+    urls: {
+      linkedin: "https://www.linkedin.com/in/manasthakur30/",
+      github: "https://github.com/Manas-thakur",
+      twitter: "https://x.com/Menace_thakur",
+    },
     image: Manas,
   },
   {
     name: "Vidushi",
-    position: "AI Team",
+    position: "AIML Team",
     intro:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet quisquam fugiat eum nulla, accusamus rerum.",
+    urls: {
+      linkedin: "https://www.linkedin.com/in/vidushi-anand-49420928a",
+      github: "https://github.com/Vidushi2709",
+      twitter: "https://x.com/Vidushi_Anand_",
+    },
     image: Vidushi,
   },
   {
@@ -96,6 +130,10 @@ export default [
     position: "Photography & Social Media",
     intro:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet quisquam fugiat eum nulla, accusamus rerum.",
+    urls: {
+      github: "https://www.github.com/kunalverma06",
+      linkedin: "https://www.linkedin.com/in/kunal-verma-528431291",
+    },
     image: Kunal,
   },
   {
@@ -103,6 +141,42 @@ export default [
     position: "Photography & Social Media",
     intro:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet quisquam fugiat eum nulla, accusamus rerum.",
+    urls: {
+      linkedin: "https://www.linkedin.com/in/tanmayverma190605",
+      github: "https://github.com/Tanmay1906",
+    },
     image: Tanmay,
+  },
+  {
+    name: "Yash Kumar",
+    position: "DSA Team",
+    intro:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet quisquam fugiat eum nulla, accusamus rerum.",
+    image: Yash,
+  },
+  {
+    name: "Saurabh Singh",
+    position: "DSA Team",
+    intro:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet quisquam fugiat eum nulla, accusamus rerum.",
+    urls: {
+      github: "https://github.com/sa1-1rabh",
+      linkedin: "https://www.linkedin.com/in/sa1-1rabh",
+    },
+    image: Saurabh,
+  },
+  {
+    name: "Dipti",
+    position: "DSA Team",
+    intro:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet quisquam fugiat eum nulla, accusamus rerum.",
+    image: Dipti,
+  },
+  {
+    name: "Divyansh Sethi",
+    position: "DSA Team",
+    intro:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet quisquam fugiat eum nulla, accusamus rerum.",
+    image: Divyansh,
   },
 ];
--- src/pages/Aboutus.tsx
+++ src/pages/Aboutus.tsx
@@ -1,42 +1,50 @@
 const AboutUs = () => {
   return (
-    <div className="about-us font-sans bg-gradient-to-b from-black via-gray-900 to-black text-white mt-[100vh]">
-      <div
-        className="h-96 flex flex-col justify-center items-center text-center bg-cover bg-center bg-no-repeat"
-        style={{ backgroundImage: "url(/path/to/image1.jpg)" }}
-      >
+    <div className="about-us font-sans from-black text-white mt-[100vh]">
+      <div className="h-96 flex flex-col justify-center items-center text-center bg-cover bg-center bg-no-repeat">
         <div className="bg-black bg-opacity-60 w-full h-full flex flex-col justify-center items-center">
           <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold">
-            Welcome to Our Club
+            About us
           </h1>
-          <p className="mt-4 text-lg md:text-xl lg:text-2xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum veritatis debitis fuga eaque totam eveniet voluptatum quibusdam, explicabo et delectus dicta corrupti temporibus ipsa quae. Dolores culpa beatae mollitia ea natus. Nihil pariatur inventore veritatis adipisci debitis voluptatum. Architecto non repellat tempora numquam eveniet omnis dolor praesentium doloribus, in amet.
+          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mt-20">
+            Welcome to Deviators
+          </h1>
+          <p className="mt-4 text-lg md:text-xl lg:text-xl xl:w-[70%]">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
+            veritatis debitis fuga eaque totam eveniet voluptatum quibusdam,
+            explicabo et delectus dicta corrupti temporibus ipsa quae. Dolores
+            culpa beatae mollitia ea natus. Nihil pariatur inventore veritatis
+            adipisci debitis voluptatum. Architecto non repellat tempora numquam
+            eveniet omnis dolor praesentium doloribus, in amet.
           </p>
         </div>
       </div>
 
-      <section className="py-12 px-4 text-center bg-gradient-to-b from-gray-800 to-gray-900">
+      <section className="py-12 px-4 text-center">
         <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-4">
           Our Mission
+          <div className="text-xl flex flex-wrap justify-center items-center">
+            {Array(5)
+              .fill(0)
+              .map((_, index) => (
+                <div
+                  key={index}
+                  className="text-xl flex flex-col w-[400px] m-20"
+                >
+                  <div className="flex justify-center text-4xl space-x-2 opacity-70">
+                    <p>01</p>
+                    <h2>Briefing</h2>
+                  </div>
+                  <p className="text-base">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Modi possimus sint dolorum qui voluptates natus dolore
+                    praesentium, deserunt eligendi eos!
+                  </p>
+                </div>
+              ))}
+          </div>
         </h2>
-        <p className="text-lg md:text-xl lg:text-2xl text-gray-300">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum veritatis debitis fuga eaque totam eveniet voluptatum quibusdam, explicabo et delectus dicta corrupti temporibus ipsa quae. Dolores culpa beatae mollitia ea natus. Nihil pariatur inventore veritatis adipisci debitis voluptatum. Architecto non repellat tempora numquam eveniet omnis dolor praesentium doloribus, in amet.
-        </p>
       </section>
-
-      <div
-        className="h-96 flex flex-col justify-center items-center text-center bg-cover bg-center bg-no-repeat"
-        style={{ backgroundImage: "url(/path/to/image2.jpg)" }}
-      >
-        <div className="bg-black bg-opacity-60 w-full h-full flex flex-col justify-center items-center">
-          <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold">
-            Our Activities
-          </h2>
-          <p className="mt-4 text-lg md:text-xl lg:text-2xl">
-           Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum veritatis debitis fuga eaque totam eveniet voluptatum quibusdam, explicabo et delectus dicta corrupti temporibus ipsa quae. Dolores culpa beatae mollitia ea natus. Nihil pariatur inventore veritatis adipisci debitis voluptatum. Architecto non repellat tempora numquam eveniet omnis dolor praesentium doloribus, in amet.
-          </p>
-        </div>
-      </div>
     </div>
   );
 };
